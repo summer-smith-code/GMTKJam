019b775b7182aa60647509f3d1fdb2e071002d78
Summer Smith <summersmith2676@gmail.com>
2026-07-26 09:54:25 -0400

    connect cinematic to game

--- GMTKJam/Assets/Scripts/MainMenu.cs
+++ GMTKJam/Assets/Scripts/MainMenu.cs
@@ -23,7 +23,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Cinematic");
     }
 
     public void QuitGame()
