5acafb3e38cd62b33cc39a14167f14353924949e
Summer Smith <summersmith2676@gmail.com>
2026-07-26 10:54:23 -0400

    interaction with antidote works

--- GMTKJam/Assets/Scripts/AntidoteIngredient.cs
+++ GMTKJam/Assets/Scripts/AntidoteIngredient.cs
@@ -1,7 +1,39 @@
 using UnityEngine;
 
-public class AntidoteIngredient : MonoBehaviour
+public class AntidoteIngredient : MonoBehaviour, IInteractable
 {
+
+    public Transform parent;
+    public bool LockCamera { get; set; } = false;
+
+    public void Click()
+    {/*
+        Debug.Log("Antidote click!");
+        parent = transform.parent;
+        if (parent.GetComponent<IInteractable>() != null)
+            parent.GetComponent<IInteractable>().Click();
+        else if (parent.GetChild(0).GetComponent<IInteractable>() != null)
+            parent.GetChild(0).GetComponent<IInteractable>().Click();
+        */
+    }
+
+    public void Interact()
+    {
+        parent = transform.parent;
+        if (parent.name.Contains("bookshelf"))
+        {
+            return;
+        }
+        if (parent.GetComponent<IInteractable>() != null )
+        parent.GetComponent<IInteractable>().Interact();
+        else if (parent.GetChild(0).GetComponent<IInteractable>() != null)
+            parent.GetChild(0).GetComponent<IInteractable>().Interact();
+    }
+
+    public void OnEnable()
+    {
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
