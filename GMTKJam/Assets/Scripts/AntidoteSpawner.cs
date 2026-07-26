e77c9d4c946989b1667989b4f06a3972ee7dcaee
Summer Smith <summersmith2676@gmail.com>
2026-07-25 22:20:49 -0400

    work on spawner

--- GMTKJam/Assets/Scripts/AntidoteSpawner.cs
+++ GMTKJam/Assets/Scripts/AntidoteSpawner.cs
@@ -5,34 +5,40 @@ using UnityEngine.UIElements;
 public class AntidoteSpawner : MonoBehaviour
 {
 
-    public GameObject[] _spawnPoints;
+    public GameObject[] _mintSpawnPoints;
+    public GameObject[] _magnesiumSpawnPoints;
+    public GameObject[] _limeSpawnPoints;
+    public GameObject[] _bottleSpawnPoints;
 
-    public GameObject[] _antidoteIngredients;
+    public GameObject _mint;
+    public GameObject _magnesium;
+    public GameObject _lime;
+    public GameObject _bottle;
+
+    public Dictionary<GameObject, GameObject[]> antidotes = new Dictionary<GameObject, GameObject[]>();
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        List<int> _used = new List<int>();
+        antidotes.Add(_lime, _limeSpawnPoints);
+        antidotes.Add(_bottle, _bottleSpawnPoints);
+        antidotes.Add(_magnesium, _magnesiumSpawnPoints);
+        antidotes.Add(_mint, _mintSpawnPoints);
 
-        if (_spawnPoints.Length > 0 && _antidoteIngredients.Length > 0 && _spawnPoints.Length >= _antidoteIngredients.Length)
+        foreach (KeyValuePair<GameObject, GameObject[]> key in antidotes)
         {
-            foreach (GameObject ingredient in _antidoteIngredients)
-            {
-                int randomIndex = Random.Range(0, _spawnPoints.Length);
-                if (_used.Contains(randomIndex))
-                {
-                    while (_used.Contains(randomIndex))
-                    {
-                        randomIndex = Random.Range(0, _spawnPoints.Length);
-                    }
-                }
-                _used.Add(randomIndex);
-                GameObject antidoteIngredient = Instantiate(ingredient, _spawnPoints[randomIndex].transform.position, Quaternion.identity);
-                antidoteIngredient.transform.parent = _spawnPoints[randomIndex].transform.parent;
-            }
+            Spawn(key.Key, key.Value);
         }
+    }
+
+
+    public void Spawn(GameObject antidote, GameObject[] spawnPoints)
+    {
+        int randomIndex = Random.Range(9, spawnPoints.Length);
+        GameObject _antidote = Instantiate(antidote, spawnPoints[randomIndex].transform.position, Quaternion.identity);
+        _antidote.transform.parent = spawnPoints[randomIndex].transform.parent;
     }
 
     // Update is called once per frame
